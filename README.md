346ba57d96877049d5f6e4b7954e90f8998b5f81
Petr Viktorin <encukou@gmail.com>
2015-03-27 10:28:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This creates header images for events.
+
+Usage: $0 IMAGE OUTPUT
+
+Sample input and output images are included. The output was generated with:
+
+    ./overlay.sh logo.png demo.png
---
+++ overlay.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+TOPIC_IMAGE=$1
+OUTPUT=$2
+
+OVERLAY_IMAGE="overlay.png"
+WINDOW_SIZE="123x98"        # {w}x{h}
+WINDOW_OFFSET="+37+70"      # +{x}+{y}
+
+if [ "$OUTPUT" = "" ]; then
+    echo "Usage: $0 IMAGE OUTPUT"
+    exit
+fi
+
+convert_cmd="
+    convert
+    $TOPIC_IMAGE
+    -resize $WINDOW_SIZE^
+    -gravity center
+    -crop $WINDOW_SIZE+0+0
+    png:-
+"
+
+composite_cmd="
+    composite
+    -compose Dst_Over
+    -gravity northwest
+    -
+    -geometry $WINDOW_OFFSET $OVERLAY_IMAGE
+    $OUTPUT
+"
+
+
+set -ex
+set -o pipefail
+
+$convert_cmd | $composite_cmd
